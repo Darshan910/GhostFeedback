1d6c970295f861943e6b2b7ab7f6ddf3dd28f1bb
Darshan910 <ui21ec07@iiitsurat.ac.in>
2024-07-22 23:38:07 +0530

    GhostFeedback

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,15 +13,16 @@ export async function middleware(request: NextRequest) {
         (
             url.pathname.startsWith('/sign-in') ||
             url.pathname.startsWith('/sign-up') ||
-            url.pathname.startsWith('/verify') ||
-            url.pathname.startsWith('/')
+            url.pathname.startsWith('/verify') 
+            // ||
+            // url.pathname.startsWith('/')
         )
     ) {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
 
     if (!token && url.pathname.startsWith('/dashboard')) {
-        return NextResponse.redirect(new URL('/sign-in', request.url));
+        return NextResponse.redirect(new URL('/', request.url));
     }
 
     return NextResponse.next();
